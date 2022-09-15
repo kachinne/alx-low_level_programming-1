3e42235057644cf360b9fc03c7f93a129cbb5f4f
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-15 16:43:53 -0700

    More Functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * * print_numbers -> print numbers
+ * print_numbers -> print numbers
  */
 
 void print_numbers(void)
@@ -9,7 +9,7 @@ void print_numbers(void)
 	int x;
 
 	for (x = 0; x < 10; x++)
-		_putchar(x + '0');
+	_putchar(x + '0');
 	_putchar('\n');
 }
 
