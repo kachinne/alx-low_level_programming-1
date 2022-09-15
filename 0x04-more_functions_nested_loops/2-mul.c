e0aa0983a3149a0bbb83afece8a9d9861f1c692f
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-15 16:37:24 -0700

    More Functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * mul -> multiplying 2 numbers
- * * @a: interger param1
- * * @b: integer param 2
- * * Return: success and intiger
+ * mul -> multiplying 2 numbers
+ * @a: interger param1
+ * @b: integer param 2
+ * Return: success and intiger
  */
 
 int mul(int a, int b)
