d699690069e83c12112cd2aa2200beda600077a3
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-16 00:26:06 +0100

    More Functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _isupper - check uppercase alphabets
  * @c: character
  * Return: 1 if c is uppercase otherwise 0.
  */
+ 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
