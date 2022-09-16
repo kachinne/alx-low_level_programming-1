07af467d0e7c4ebe87a460d5a6b571eee178f6ea
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-15 17:00:26 -0700

    More Functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * * print_most_numbers -> this prints most numbers
+ * print_most_numbers -> this prints most numbers
  */
 void print_most_numbers(void)
 {
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 	for (x = 0; x < 10; x++)
 	{
 		if (x != 2 && x != 4)
-			_putchar(x + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
