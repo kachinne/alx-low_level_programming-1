7ee1b42e4891154a2a356505bebd0a26c98544af
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-15 17:11:17 -0700

    More Function

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * * print_square -> printing line
- * * @size: integer params
+ * print_square -> printing line
+ * @size: integer params
  */
 void print_square(int size)
 {
 	int x, y;
 
 	if (size <= 0)
-		_putchar('\n');
+	_putchar('\n');
 	else
 	{
 		for (x = 0; x < size; x++)
 		{
 			for (y = 0; y < size; y++)
 			{
-				_putchar(35);
+			_putchar(35);
 			}
 			_putchar('\n');
 		}
