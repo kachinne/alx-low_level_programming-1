1a1769573099ed5522836afb6cd5e432e038b776
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-09-26 21:17:16 +0100

    More PoArrStr

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
-include "main.h"
+#include "main.h"
 
-/**
+ /**
   * _strpbrk - search a string for any of a set of bytes
   * @s: source string
   * @accept: accepted characters
-  *
   * Return: the string since the first found accepted character
   */
+
 char *_strpbrk(char *s, char *accept)
 {
 	int a = 0, b;
