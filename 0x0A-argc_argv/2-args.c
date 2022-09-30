3ad292975354371fbe99b9c4ba7653bd0dbeacc6
Precious Okwukwe Amaechi <Precious Okwukwe Amaechi>
2022-09-30 14:45:36 +0100

    ArgC_ArgV

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - this is a function to print its name
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
-include <stdio.h>
-include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - this is a function to print its name
